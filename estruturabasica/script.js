31ea5bce30ba0723c552eacec7c6f1e5562dbf27
Ugo136 <ugo.mram@gmail.com>
2023-06-16 12:15:27 -0300

    Ponto Final

--- estruturabasica/script.js
+++ estruturabasica/script.js
@@ -1,3 +1,9 @@
-const inputb = document.getElementById("inputb")
-const botao = document.getElementById("inputb")
+const inputb = document.getElementById("inputb");
+const botao = document.getElementById("botao");
 
+let nome = '';
+
+botao.onclick =()=>{
+    nome = inputb.value
+    alert("esse é o seu nome:" + nome);
+}
